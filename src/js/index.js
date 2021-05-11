e9b544e622dc294da526515b141749369fc55e76
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-05-11 20:55:22 -0300

    errase

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,4 @@
+const userPage = './lista-usuarios.html'
 var botaoEntrar = document.querySelector("#entrar");
 
 botaoEntrar.addEventListener("click", async function login (event) {
@@ -24,7 +25,7 @@ botaoEntrar.addEventListener("click", async function login (event) {
       let token = data.token;
       if (token !== undefined && token !== null) {
         sessionStorage.setItem('token', token)
-        userList();
+        window.location.href = userPage;        
       } else {
         removeSpinner();
         setErrorMessage('Usuário ou senha incorretos');
@@ -33,7 +34,7 @@ botaoEntrar.addEventListener("click", async function login (event) {
     //var a = response.json();
 });
 
-async function userList(token) {
+/*async function userList(token) {
 
   await fetch('https://reqres.in/api/users?page=1', {
       method: 'GET',
@@ -42,9 +43,33 @@ async function userList(token) {
       }
   })
   .then((response) => response.json())  
-  .then((data) => {
-    let page = data.page;
-    console.log(page)    
+  .then((data) => {      
+      let showing = data.per_page;
+      let total = data.total;
+      let users = data.data;      
+      let displayInfoString = 'Mostrando ' + showing + ' de ' + total + '';
+      
+      for (let i = 0; i < users.length; i++) {
+        
+        '<div class="user-item">' +
+        `<img class="user-img" src=${
+        users[i].avatar}
+        >` +
+        '<p class="user-name">' +
+        `${users[i].first_name}` +
+        ' ' +
+        `${users[i].last_name}` +
+        '</p>' +
+        '<p class="user-email">' +
+        `${users[i].email}` +
+        '</p>' +
+        '<img class="edit-icon" src="../assets/icon-edit.svg">' +
+        '</div>';
+      }
+        const container = document.querySelector('#list-container');
+        container.insertAdjacentHTML('beforeend', userCard);
+        const displayInfo = document.querySelector('#list-info');
+        displayInfo.insertAdjacentHTML('beforeend', displayInfoString);  
   });
   
-};
+};*/
