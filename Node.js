cfdcd6b8c29b93540397719c84d55bf8c76c4d9b
Sérgio Schwartz Varejão <43224989+varejaosergio@users.noreply.github.com>
2021-05-12 14:05:59 -0300

    error

--- Node.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-var data = require('./src/js/index');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(data);
-  res.end();
-}).listen(8080);
